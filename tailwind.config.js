bbb622a4db192e640ef32d5a311e95038fb604e7
jaw <62795911+JacobWestby@users.noreply.github.com>
2023-01-11 05:52:28 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["public/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#36C2FD",
+      secondary: "#323232",
+      accent: "#D88637",
+      white: "#FFFEFD",
+      black: "#000607",
+      overlay: "rgba(30, 30, 30, 0.79)",
+    }
+  },
+  plugins: [],
+}
